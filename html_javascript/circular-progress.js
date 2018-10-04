5fd87136b2ad8a624903dc6849bf84ed337a3dc5
Azi Crawford <azi.crawford@gonitro.com>
2018-10-04 14:21:22 +0100

    enables going from lower arbitrary percent to higher one

--- html_javascript/circular-progress.js
+++ html_javascript/circular-progress.js
@@ -3,12 +3,15 @@
  * @Desc: Shows progress bar.
  * @Params: _upto -> upto that percentage.
  */
-function __showProgress(_upto, _cir_progress_id) {
+function __showProgress(_from, _upto, _cir_progress_id) {
 
     //Filter Percentage
     _upto = (_upto > 100) ? 100 : ((_upto < 0) ? 0 : _upto);
 
-    var _progress = 0;
+    //Filter Percentage
+    _from = (_from > 100) ? 100 : ((_from < 0) ? 0 : _from);
+
+    var _progress = _from;
 
     var _cir_progress = document.getElementById(_cir_progress_id).getElementsByClassName("_cir_P_y")[0];
     var _text_percentage = document.getElementById(_cir_progress_id).getElementsByClassName("_cir_Per")[0];
